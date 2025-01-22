e3f24d7da54b00fc5f421f7b56631c6706ca20fb
Alexis Meniante <alexismeniante@MacBook-Air-de-Alexis.local>
2025-01-22 15:22:15 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     }
   },
+  base: '/mycitymove-platform/',
   build: {
     outDir: 'dist',
     sourcemap: true,
@@ -42,5 +43,4 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
-  base: '/',
 });
